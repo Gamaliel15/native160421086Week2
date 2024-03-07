90713d9ba6c02daa70cdf243991b965ab1bdd80c
Gamaliel15 <s160421086@student.ubaya.ac.id>
2024-03-07 15:01:15 +0700

    init week3

--- app/src/main/java/com/gamaliel/adv160421086week2/MainActivity.kt
+++ app/src/main/java/com/gamaliel/adv160421086week2/MainActivity.kt
@@ -5,21 +5,29 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.gamaliel.adv160421086week2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         navController = (supportFragmentManager.findFragmentById(R.id.navHost) as NavHostFragment).navController
 
-        NavigationUI.setupActionBarWithNavController(this,navController)
+        NavigationUI.setupActionBarWithNavController(this,navController,binding.drawerLayout)
+
+        NavigationUI.setupWithNavController(binding.navView, navController)
+
+        binding.bottomNav.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, binding.drawerLayout) || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/gamaliel/adv160421086week2/MainFragment.kt
+++ app/src/main/java/com/gamaliel/adv160421086week2/MainFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.navigation.Navigation
 import com.gamaliel.adv160421086week2.databinding.FragmentMainBinding
 
@@ -12,6 +13,7 @@ import com.gamaliel.adv160421086week2.databinding.FragmentMainBinding
 class MainFragment : Fragment() {
 
     private lateinit var binding: FragmentMainBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,11 +27,18 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.btnSetting.setOnClickListener{
+            val action = MainFragmentDirections.actionOptionFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+
         binding.btnStart.setOnClickListener {
             val name = binding.txtName.text.toString()
             val action = MainFragmentDirections.actionGameFragment(name)
             Navigation.findNavController(it).navigate(action)
 
         }
+
+
     }
 }
